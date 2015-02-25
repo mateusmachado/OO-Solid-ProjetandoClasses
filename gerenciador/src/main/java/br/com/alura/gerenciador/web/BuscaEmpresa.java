e627db2e65e9a7147fc74e15af1616f17b0bb7e2
Mateus <mmachado.csp@gmail.com>
2015-02-24 22:01:22 -0300

    Finish First class

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -2,12 +2,18 @@ package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.gerenciador.Empresa;
+import br.com.alura.gerenciador.dao.EmpresaDAO;
+
+@WebServlet(urlPatterns = "/busca")
 public class BuscaEmpresa extends HttpServlet {
 
 	@Override
@@ -17,5 +23,14 @@ public class BuscaEmpresa extends HttpServlet {
 		writer.println("<html><body>");
 		writer.println("Resultado da busca <br/>");
 		writer.println("<html><body>");
+		String filtro = req.getParameter("filtro");
+		Collection<Empresa> empresas = new EmpresaDAO()
+				.buscaPorSimilaridade(filtro);
+		writer.println("<ul>");
+		for (Empresa empresa : empresas) {
+			writer.println("<li>" + empresa.getId() + ": " + empresa.getNome());
+		}
+		writer.println("</ul>");
+		writer.println("<body></html>");
 	}
 }
