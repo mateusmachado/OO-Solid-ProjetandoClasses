385c59896c3e8f1ad4131bdd07d3a30f88cc25a9
Mateus <mmachado.csp@gmail.com>
2015-03-05 21:52:57 -0300

    First

---
+++ OO-Solid-ProjetandoClasses/src/br/com/main/Main.java
@@ -0,0 +1,16 @@
+package br.com.main;
+
+import br.com.test.Test;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		Test t = new Test();
+		t.setX("first");
+		System.out.println(t.getX());
+		
+	}
+
+}
---
+++ OO-Solid-ProjetandoClasses/src/br/com/test/Test.java
@@ -0,0 +1,15 @@
+package br.com.test;
+
+public class Test {
+	
+	String x;
+
+	public String getX() {
+		return x;
+	}
+
+	public void setX(String x) {
+		this.x = x;
+	}
+
+}
